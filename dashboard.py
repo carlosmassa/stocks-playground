a316392948d742e54bcce023120ec68c471d4358
Carlos Massa <47972273+carlosmassa@users.noreply.github.com>
2021-04-11 16:41:41 +0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -339,36 +339,4 @@ fig.update_layout(
 )
 st.plotly_chart(fig, use_container_width=True)
 
-x = [1, 2, 3]
-y = [1000, 10000, 100000]
-y2 = [5000, 10000, 90000]
-
-trace1 = go.Bar(x=x, y=y, name='trace1')
-trace2 = go.Bar(x=x, y=y2, name='trace2', visible=False)
-
-
-data = [trace1, trace2]
-
-updatemenus = list([
-    dict(active=1,
-         buttons=list([
-            dict(label='Log Scale',
-                 method='update',
-                 args=[{'visible': [True, True]},
-                       {'title': 'Log scale',
-                        'yaxis': {'type': 'log'}}]),
-            dict(label='Linear Scale',
-                 method='update',
-                 args=[{'visible': [True, False]},
-                       {'title': 'Linear scale',
-                        'yaxis': {'type': 'linear'}}])
-            ]),
-        )
-    ])
-
-layout = dict(updatemenus=updatemenus, title='Linear scale')
-fig = go.Figure(data=data, layout=layout)
-
-st.plotly_chart(fig, use_container_width=True)
-
 
