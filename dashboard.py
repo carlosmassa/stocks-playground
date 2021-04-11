bca6adae21da364b04677cde6e5d4e78b04439cb
Carlos Massa <47972273+carlosmassa@users.noreply.github.com>
2021-04-11 17:24:56 +0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -243,7 +243,7 @@ fig.update_layout(
             x0="2009-01-03",
             x1="2012-11-28",
             xref="x",
-            y0=0,
+            y0=min(trace.y),
             y1=1,
             yref="paper"
         ),
