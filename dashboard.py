03b113dc813cc9fc040b9f15010e30054f8afeb6
Carlos Massa <47972273+carlosmassa@users.noreply.github.com>
2021-04-12 14:05:50 +0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -435,7 +435,7 @@ df['MinPriceUSD200days'] = df['PriceUSD'].rolling(window=200).min()
 df['MinPriceUSD365days'] = df['PriceUSD'].rolling(window=365).min()
 df['MinPriceUSD2years'] = df['PriceUSD'].rolling(window=730).min()
 df['MinPriceUSD4years'] = df['PriceUSD'].rolling(window=1460).min()
-df['AllMinPriceEqual'] = np.where(((df['MinPriceUSD50days'] == df['MinPriceUSD100days']) & (df['MinPriceUSD50days'] == df['MinPriceUSD365days'])), df['date'], 0)
+df['AllMinPriceEqual'] = np.where(((df['MinPriceUSD50days'] == df['MinPriceUSD100days']) & (df['MinPriceUSD50days'] == df['MinPriceUSD200days']) & (df['MinPriceUSD50days'] == df['MinPriceUSD365days'])), df['date'], 0)
 
 # Calculation of lowest price forward
 # reverse df
@@ -551,6 +551,7 @@ st.write(firstday_bottoms_copy[0])
 # add lines using absolute references
 for k in range(len(firstday_bottoms)):
     st.write(k)
+    st.write(firstday_bottoms[k])
     fig.add_shape(type='line',
                 yref="y",
                 xref="x",
